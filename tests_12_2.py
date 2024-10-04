5a8eedc3af7033d40b8eb21ddc171d8ec0e9d523
DieSonne-1 <ufimtseva.po@gmail.com>
2024-10-04 19:04:35 +0500

    Add files via upload

---
+++ tests_12_2.py
@@ -0,0 +1,38 @@
+import module_12_2 as md
+import unittest
+
+class TournamentTest(unittest.TestCase):
+    @classmethod
+    def SetUpClass(cls):
+        cls.all_results = {}
+
+    def setUp(self):
+        self.first = md.Runner('Усэйн', 10)
+        self.second = md.Runner('Андрей', 9)
+        self.third = md.Runner('Ник', 3)
+
+    def test_first_tournament(self):
+        tournament = md.Tournament(90, self.first, self.third)
+        result = tournament.start()
+        self.all_results['1'] = result
+        self.assertTrue(result[2] == 'Ник')
+
+    def test_second_tournament(self):
+        tournament = md.Tournament(90, self.second, self.third)
+        result = tournament.start()
+        self.all_results['2'] = result
+        self.assertTrue(result[2] == 'Ник')
+
+    def test_third_tournament(self):
+        tournament = md.Tournament(90, self.first, self.second, self.third)
+        result = tournament.start()
+        self.all_results['3'] = result
+        self.assertTrue(result[3] == 'Ник')
+
+    @classmethod
+    def tearDownClass(cls):
+        for k,v in cls.all_results.items():
+            print(f'{k}:{v}')
+
+
+
